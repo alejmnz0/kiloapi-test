d7c7e16456212f31cf41c5c5641a184721edc810
A.Jimenez <jimenezalejandro505@gmail.com>
2024-01-19 14:29:23 +0100

    asdas

--- src/test/java/com/salesianostriana/kilo/ClaseServiceTest/ClaseServiceTest.java
+++ src/test/java/com/salesianostriana/kilo/ClaseServiceTest/ClaseServiceTest.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.kilo.ClaseServiceTest;
 
+import com.salesianostriana.kilo.entities.Aportacion;
 import com.salesianostriana.kilo.entities.Clase;
 import com.salesianostriana.kilo.repositories.ClaseRepository;
 import com.salesianostriana.kilo.services.ClaseService;
@@ -7,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 public class ClaseServiceTest {
 
@@ -24,10 +27,22 @@ public class ClaseServiceTest {
             Clase.builder()
                     .nombre("Frutos secos")
                     .tutor("Luismi")
+                    .aportaciones(Set.of(Aportacion.builder()
+                            .fecha(LocalDate.now()).build()))
                     .build(),
                 Clase.builder()
                         .nombre("Femenino")
                         .tutor("Miguel")
+                        .aportaciones(Set.of(Aportacion.builder()
+                                .fecha(LocalDate.now()).build()))
+                        .build(),
+                Clase.builder()
+                        .nombre("Colectivo")
+                        .tutor("Jesus")
+                        .aportaciones(Set.of(Aportacion.builder()
+                                .fecha(LocalDate.now()).build(),
+                                Aportacion.builder()
+                                        .fecha(LocalDate.now()).build()))
                         .build()
         );
     }
